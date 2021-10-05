a2cda4801444ede556178e01021ebf3a627d6bda
ashtilawat23 <ashtilawat23@gmail.com>
2021-10-05 12:55:39 -0400

    Promise handling for multiple repos.

--- index.js
+++ index.js
@@ -5,13 +5,44 @@ const octokit = new Octokit({
     auth: process.env.PERSONAL_GITHUB_ACCESS_TOKEN,
 });
 
-octokit.rest.repos.listBranches({
-    owner: "Lambda-School-Labs",
-    repo: "family-promise-service-tracker-fe-a"
-})
-.then( (res) => {
-    console.log(res.data);
-})
-.catch( (err) => {
-    console.log(err);
-});
+const org = "Lambda-School-Labs";
+const test_repo = "family-promise-service-tracker-fe-a";
+let shaArray = [];
+
+// // Code to log commit sha's from the GitHub API 
+// octokit.rest.repos.listCommits({
+//     owner: "Lambda-School-Labs",
+//     repo: "family-promise-service-tracker-fe-a"
+// })
+// .then( (res) => {
+//     res.data.forEach((commit) => {
+//         console.log(commit.sha);
+//     });
+// })
+// .catch( (err) => {
+//     console.log(err);
+// });
+
+async function getCommitsbyRepo(owner, repo) {
+    const commits = await octokit.rest.repos.listCommits({
+        owner: owner,
+        repo: repo
+    });
+    return commits;
+}
+
+
+Promise.all([getCommitsbyRepo(org, test_repo)])
+    .then((res) => {
+        res.forEach((repo) => {
+            repo.data.map((commit) => {
+                commit.sha = [...shaArray];
+            })
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+    console.log(shaArray);
+
